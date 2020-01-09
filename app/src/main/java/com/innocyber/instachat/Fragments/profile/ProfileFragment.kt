6e0d3696d1d871e04941452225bc4621b3a6f36c
innocyber <innocentileka@gmail.com>
2020-01-09 16:42:49 +0100

    Edit profile UI design

--- app/src/main/java/com/innocyber/instachat/Fragments/profile/ProfileFragment.kt
+++ app/src/main/java/com/innocyber/instachat/Fragments/profile/ProfileFragment.kt
@@ -1,13 +1,17 @@
 package com.innocyber.instachat.Fragments.profile
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.innocyber.instachat.AccountSettingsActivity
 
 import com.innocyber.instachat.R
+import kotlinx.android.synthetic.main.fragment_profile.*
+import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -19,7 +23,13 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        view.edit_account_settings.setOnClickListener{
+            startActivity(Intent(context,AccountSettingsActivity::class.java))
+        }
+
+        return view
     }
 
 
